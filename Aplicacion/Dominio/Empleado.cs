8c594c312ba171bb03e54cec021b68a3d9b16a5d
Willington Londoño Ramos <wlondono@carecloud.com.co>
2021-10-05 18:14:25 -0500

    Se agrega select List

--- Aplicacion/Dominio/Empleado.cs
+++ Aplicacion/Dominio/Empleado.cs
@@ -1,11 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 namespace Dominio
 {
     public class Empleado
     {
+        
         public int Id {get; set;}
         public string Nombre {get; set;}
         public string Apellidos {get; set;}
         public string Cedula {get; set;}
+        [Required(ErrorMessage="El campo Código de empleado es requerido")]
         public int CodigoEmpleado {get; set;}
         public NombreRol NombreRol {get; set;}
         public string sucursal {get; set;}
