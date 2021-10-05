0f0317b0a5df2c83747470fe05a62bcd2b1745d7
Nicolas Rojas <nicrojasc6@gmail.com>
2021-10-05 18:18:45 -0500

    Comentario Nicolas

--- Aplicacion/Dominio/Empleado.cs
+++ Aplicacion/Dominio/Empleado.cs
@@ -3,7 +3,7 @@ namespace Dominio
 {
     public class Empleado
     {
-        
+        //atributos
         public int Id {get; set;}
         public string Nombre {get; set;}
         public string Apellidos {get; set;}
